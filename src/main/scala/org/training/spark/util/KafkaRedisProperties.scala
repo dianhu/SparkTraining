51f99e45896c31acf493051975dd2ad5a80b9a13
huchengyuan <huchengyuan@huoyunren.com>
2016-11-03 17:39:17 +0800

    新增一些例子

---
+++ src/main/scala/org/training/spark/util/KafkaRedisProperties.scala
@@ -0,0 +1,17 @@
+package org.training.spark.util
+
+/**
+  * Time : 16-11-3 下午5:37
+  * Author : hcy
+  * Description : 
+  */
+object KafkaRedisProperties {
+  val REDIS_SERVER: String = "chinahadoop-1"
+  val REDIS_PORT: Int = 6379
+
+  val KAFKA_SERVER: String = "chinahadoop-1"
+  val KAFKA_ADDR: String = KAFKA_SERVER + ":9092"
+  val KAFKA_USER_TOPIC: String = "user_events"
+  val KAFKA_RECO_TOPIC: String = "reco4"
+
+}
